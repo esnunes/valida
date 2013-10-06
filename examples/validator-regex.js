6fec85f42ec18ef3bb2f316b36687b374b4883a8
Eduardo Nunes <esnunes@gmail.com>
2013-10-06 16:52:11 -0300

    fixed regex validator name

--- examples/validator-regex.js
+++ examples/validator-regex.js
@@ -8,7 +8,7 @@ var schema = {
     { validator: Valida.Validator.regex, pattern: /^[0-9]*$/, groups: ['not matching pattern'] },
     { validator: Valida.Validator.regex, pattern: /^a[0-9]*z$/, groups: ['matching pattern'] },
     { validator: Valida.Validator.regex, pattern: '^a[0-9]*z$', groups: ['matching string without modifiers'] },
-    { validator: Valida.Validator.regex, pattern: '[A-Z]', modifiers: 'i', groups: ['matching string with modifiers'] }
+    { validator: Valida.Validator.regex, pattern: '[A-Z]', modifiers: 'i', groups: ['string with modifiers'] }
   ]
 };
 
@@ -36,7 +36,14 @@ Valida.process(data, schema, function(err, ctx) {
   if (err) return console.log(err);
   if (!ctx.isValid()) return console.log('invalid', ctx.errors());
   console.log('valid');
-}, 'matching string with modifiers');
+}, 'string with modifiers');
+
+data.code = '123';
+Valida.process(data, schema, function(err, ctx) {
+  if (err) return console.log(err);
+  if (!ctx.isValid()) return console.log('invalid', ctx.errors());
+  console.log('valid');
+}, 'string with modifiers');
 
 
 data.code = null;
