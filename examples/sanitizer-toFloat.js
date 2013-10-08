1aefc24b22a2588bb30d4edb412e0d90e3f70a7f
Eduardo Nunes <esnunes@gmail.com>
2013-10-07 21:19:31 -0300

    added toFloat sanitizer

---
+++ examples/sanitizer-toFloat.js
@@ -0,0 +1,29 @@
+
+'use strict';
+
+var Valida = require('..');
+
+var schema = {
+  salary: [
+    { sanitizer: Valida.Sanitizer.toFloat, groups: ['without precision'] },
+    { sanitizer: Valida.Sanitizer.toFloat, precision: 6, groups: ['with precision'] }
+  ]
+};
+
+var person = { salary: '1.541712812' };
+
+Valida.process(person, schema, function(err, ctx) {
+  console.log('without precision', person);
+}, 'without precision');
+
+person.salary = 1.541712812;
+
+Valida.process(person, schema, function(err, ctx) {
+  console.log('with precision', person);
+}, 'with precision');
+
+person.salary = '12.423423134123';
+
+Valida.process(person, schema, function(err, ctx) {
+  console.log('with precision', person);
+}, 'with precision');
