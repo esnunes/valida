5b37ef75bc1589ea07add1f9859d300c26510aad
Eduardo Nunes <esnunes@gmail.com>
2013-10-08 21:30:24 -0300

    added sanitizer toDate

---
+++ examples/sanitizer-toDate.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+var Valida = require('..');
+
+var schema = {
+  birthday: [
+    { sanitizer: Valida.Sanitizer.toDate }
+  ]
+};
+
+var person = { birthday: 'Tue Oct 08 2013 19:09:56 GMT-0300 (BRT)' };
+
+Valida.process(person, schema, function(err, ctx) {
+  console.log(person, Object.prototype.toString.call(person.birthday));
+});
+
+person.birthday = new Date();
+
+Valida.process(person, schema, function(err, ctx) {
+  console.log(person, Object.prototype.toString.call(person.birthday));
+});
